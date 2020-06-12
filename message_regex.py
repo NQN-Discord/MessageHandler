c4bda10d078c98d81ad062c55a4395de3eaf2de9
Blue <redacted>
2020-06-12 23:26:05 +0100

    Write filtery thingy

--- message_regex.py
+++ message_regex.py
@@ -1,23 +1,61 @@
 from typing import List
+import re
+
+colon_regex = re.compile(
+    r"(?P<rendered_emote><a?:[a-zA-Z0-9_]+:\d+>)|"
+    r"(?P<unrendered_emote>:(?:[a-zA-Z0-9_]+-)?[a-zA-Z0-9_]+:(?!\d+>))|"
+    r"(?P<message_link>(?:https?://(?:[a-z]+\.)?)?discord(?:app)?\.com/channels/\d+/\d+/\d+)|"
+    r"(?P<sticker>(?:[a-zA-Z0-9_]+\.[a-zA-Z0-9_]+:)|(?:<a?:[a-zA-Z0-9_]+_sticker_[a-zA-Z0-9_]+:\d+>))|"
+    r"(?P<masked_url>\[.*\]\(https?://[a-zA-Z0-9:.-_]{1,256}\.[a-zA-Z0-9]{1,6}\b[-a-zA-Z0-9@:%_+.~#?&/=]*\))"
+)
 
 
-def run_profiling(messages: List):
+def message_type(content, prefix: str = "!") -> List[List[str]]:
+    if not content:
+        return []
+    if content.startswith(prefix):
+        return [["prefix", prefix, content.replace(prefix, "", 1)]]
+    elif content[:21] in {"<@559426966151757824>", "<@561541673750888481>"}:
+        return [["prefix", prefix, content[21:]]]
+    elif ":" in content:
+        return [[m.lastgroup, m.group()] for m in colon_regex.finditer(content)]
+    elif content == "@someone":
+        return [["@someone"]]
+    return []
+
+
+def run_profiling(messages: List[str]):
     # Messages I care about:
-    #
-    print(len(messages))
+    #  @someone
+    #  !prefix
+    #  Jump URLs
+    #  Masked URLs
+    #  Rendered emoji
+    #  Unrendered emoji
+
+    #  @someone
+    #  !prefix
+    #  Any message with a colon for further processing
+    for message in messages:
+        for match, *data in message_type(message):
+            message_types[match] += 1
+    print(message_types, sum(i for i in message_types.values()), len(messages))
 
 
 if __name__ == "__main__":
-    import json
-    messages = []
-    with open("messages.json") as messages_f:
-        all = messages_f.read()
-        i = 1
-        while all:
-            print("Loading part", i)
-            i += 1
 
-            data, end = json.JSONDecoder().raw_decode(all)
-            messages.extend(data)
-            all = all[end:]
+    import json as json
+    with open(r"messages.json") as messages_f:
+        messages = json.load(messages_f)
+
+    message_types = {
+        "@someone": 0,
+        "prefix": 0,
+        "rendered_emote": 0,
+        "unrendered_emote": 0,
+        "message_link": 0,
+        "sticker": 0,
+        "masked_url": 0
+    }
+
     run_profiling(messages)

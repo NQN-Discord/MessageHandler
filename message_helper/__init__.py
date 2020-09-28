4353fac63423c6a96f59ef8a15874b48e7b7f37f
Blue <redacted>
2020-09-28 19:32:18 +0100

    Migrate to being used as a library

---
+++ message_helper/__init__.py
@@ -0,0 +1,1 @@
+from .message_regex import get_message_types
---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+
+setup(
+   name="message_helper",
+   version="0.1.0",
+   description="The set of regular expressions used for filtering out unwanted messages",
+   author='Blue',
+   url="https://nqn.blue/",
+   packages=["message_helper"],
+   install_requires=[]
+)
